ae826ae3f8078cc6166e019a202214cbb1fa56a7
adumitrescu2708 <adumitrescu2708@stud.acs.upb.ro>
2023-03-19 11:57:12 +0200

    Added source files

---
+++ src/lab_m1/lab5/lab_camera.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "utils/glm_utils.h"
+#include "utils/math_utils.h"
+
+
+namespace implemented
+{
+    class Camera
+    {
+     public:
+        Camera()
+        {
+            position    = glm::vec3(0, 2, 5);
+            forward     = glm::vec3(0, 0, -1);
+            up          = glm::vec3(0, 1, 0);
+            right       = glm::vec3(1, 0, 0);
+            distanceToTarget = 2;
+        }
+
+        Camera(const glm::vec3 &position, const glm::vec3 &center, const glm::vec3 &up)
+        {
+            Set(position, center, up);
+        }
+
+        ~Camera()
+        { }
+
+        // Update camera
+        void Set(const glm::vec3 &position, const glm::vec3 &center, const glm::vec3 &up)
+        {
+            this->position = position;
+            forward     = glm::normalize(center - position);
+            right       = glm::cross(forward, up);
+            this->up    = glm::cross(right, forward);
+        }
+
+        void MoveForward(float distance)
+        {
+            // Translates the camera using the `dir` vector computed from
+            // `forward`. Movement will always keep the camera at the same
+            // height. For example, if you rotate your head up/down, and then
+            // walk forward, then you will still keep the same relative
+            // distance (height) to the ground!
+        }
+
+        void TranslateForward(float distance)
+        {
+        }
+
+        void TranslateUpward(float distance)
+        {
+        }
+
+        void TranslateRight(float distance)
+        {
+            // TODO(student): See instructions below. Read the entire thing!
+            // You need to translate the camera using the `right` vector.
+            // Usually, however, translation using camera's `right` vector
+            // is not very useful, because if the camera is rotated around the
+            // `forward` vector, then the translation on the `right` direction
+            // will have an undesired effect, more precisely, the camera will
+            // get closer or farther from the ground. The solution is to
+            // actually use the projected `right` vector (projected onto the
+            // ground plane), which makes more sense because we will keep the
+            // same distance from the ground plane.
+        }
+
+        void RotateFirstPerson_OX(float angle)
+        {
+
+
+        }
+
+        void RotateFirstPerson_OY(float angle)
+        {
+
+        }
+
+        void RotateFirstPerson_OZ(float angle)
+        {
+
+        }
+
+        void RotateThirdPerson_OX(float angle)
+        {
+
+
+        }
+
+        void RotateThirdPerson_OY(float angle)
+        {
+
+
+        }
+
+        void RotateThirdPerson_OZ(float angle)
+        {
+
+        }
+
+        glm::mat4 GetViewMatrix()
+        {
+            // Returns the view matrix
+            return glm::lookAt(position, position + forward, up);
+        }
+
+        glm::vec3 GetTargetPosition()
+        {
+            return position + forward * distanceToTarget;
+        }
+
+     public:
+        float distanceToTarget;
+        glm::vec3 position;
+        glm::vec3 forward;
+        glm::vec3 right;
+        glm::vec3 up;
+    };
+}   // namespace implemented
